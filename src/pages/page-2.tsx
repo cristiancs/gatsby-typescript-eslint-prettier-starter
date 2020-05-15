91ecf62fdf5624861e970aabc32bb5bf01bf8246
Maarten Tielemans <ttielu@gmail.com>
2020-05-16 01:16:27 +0200

    🐛 Resolved all TypeScript errors

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,17 +1,17 @@
 // Gatsby supports TypeScript natively!
-import React from "react"
-import { PageProps, Link } from "gatsby"
+import { Link, PageProps } from 'gatsby';
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from '../components/seo';
+import Layout from '../layouts/default-layout';
 
-const SecondPage = (props: PageProps) => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2 ({props.path})</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = ({ path }: PageProps): JSX.Element => (
+    <Layout>
+        <SEO title="Page two" />
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2 ({path})</p>
+        <Link to="/">Go back to the homepage</Link>
+    </Layout>
+);
 
-export default SecondPage
+export default SecondPage;
